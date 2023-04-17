1635a8e09a945609abec230a938af7baab43f3d4
gadik <112864541+gadik5551@users.noreply.github.com>
2023-04-17 17:44:57 +1100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -4579,7 +4579,7 @@ function library:init()
         if not IonHub_User then
             getgenv().IonHub_User = {
                 UID = 0, 
-                User = '*NickName*'
+                User = {'NickName', true}
             }
         end
         self.watermark = {
